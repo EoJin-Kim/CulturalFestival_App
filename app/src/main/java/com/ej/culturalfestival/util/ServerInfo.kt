0bcb583a9b9a16b0b4d2c5e1e6736aea5ceb32ca
EoJin-Kim <linexy@naver.com>
2022-09-22 11:58:34 +0900

    Feat: 서버 URL 수정

--- app/src/main/java/com/ej/culturalfestival/util/ServerInfo.kt
+++ app/src/main/java/com/ej/culturalfestival/util/ServerInfo.kt
@@ -4,6 +4,7 @@ class ServerInfo {
 
     companion object{
 //        val SERVER_URL = "http://39.118.206.158:8080"
-        val SERVER_URL = "http://10.10.20.137:8080"
+//        val SERVER_URL = "http://10.10.20.137:8080"
+        val SERVER_URL = "https://festivalkorea.herokuapp.com"
     }
 }
